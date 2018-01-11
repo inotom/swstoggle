e3c1f1f6e1c90db1598788b195d94889062c1c58
inotom <wdf7322@yahoo.co.jp>
2018-01-11 14:58:25 +0900

    Fix export module

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ const constants = require('./constants.js');
 const HANDLE_CLASS = constants.HANDLE_CLASS;
 const FOLDER_CLASS = constants.FOLDER_CLASS;
 
-for (let elHandle of document.querySelectorAll(`.${HANDLE_CLASS}`)) {
-  const elNext = elHandle.nextElementSibling;
-  const hasNextFolder = elNext.classList.contains(FOLDER_CLASS);
+module.exports = () => {
+  const handles = document.querySelectorAll(`.${HANDLE_CLASS}`);
 
-  elHandle.addEventListener('click', makeAction(elHandle, elNext, hasNextFolder));
+  for (let i = 0, len = handles.length; i < len; i++) {
+    const elHandle = handles[i];
+    const elNext = elHandle.nextElementSibling;
+    const hasNextFolder = elNext.classList.contains(FOLDER_CLASS);
 
-  if (hasNextFolder) {
-    elNext.style.height = 0;
-    elNext.style.overflow = 'hidden';
+    elHandle.addEventListener('click', makeAction(elHandle, elNext, hasNextFolder));
+
+    if (hasNextFolder) {
+      elNext.style.height = 0;
+      elNext.style.overflow = 'hidden';
+    }
   }
-}
+};
